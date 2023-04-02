735c4c32f92c26dc509885816618cca0de324a11
Claire Cressman <Claire Cressman@Tests-MacBook-Pro.local>
2023-04-02 13:38:15 -0400

    adding json files

--- Data Cleaning/Python Script/sqlite_db_and_json.py
+++ Data Cleaning/Python Script/sqlite_db_and_json.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 
+
 # %%
 #Create engine using SQLAlchemy to set up db and create sqlite file (sqlite protocol then file path)
 engine = create_engine ('sqlite:///../../resources/ghg_emissions.sqlite')
@@ -14,7 +15,7 @@ conn = engine.connect() #connect to db
 #Read in cleaned METHANE emissions data and upload to database
 methane_dataset = pd.read_csv('../../resources/methane_emissions_by_country.csv')
 methane_dataset.to_sql('country_ch4_emissions', conn, if_exists='replace', index=False) 
-
+methane_dataset.to_json('../../resources/country_ch4_emissions.json', 'records')
 
 # %%
 #read methane data back out of df to verify process complete
@@ -27,7 +28,7 @@ print(dataframe_check)
 #read in cleaned CARBON emissions data and upload to database
 carbon_dataset = pd.read_csv('../../resources/carbon_emissions_by_country.csv')
 carbon_dataset.to_sql('country_co2_emissions', conn, if_exists='replace', index=False)
-
+carbon_dataset.to_json('../../resources/country_co2_emissions.json', 'records')
 
 
 #%%
